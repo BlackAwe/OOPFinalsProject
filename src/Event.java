b1a13d756a13d84b9cc2f6e4c812769a82a3cc7c
Justin <itinjus@gmail.com>
2023-12-19 20:41:24 +0800

    Updating event class
    Fixed variables such as date and time, added constructors, getters and
    setters

--- src/Event.java
+++ src/Event.java
@@ -4,16 +4,51 @@ public class Event {
 	private int eventId;
 	private String eventName;
 	private String typeOfEvent;
-	private String date;
-	private String time;
+	private String dateAndTime;
 	private String venue;
 	private int noOfParticipants;
 	private String description;
 	private String status;
 	
 	// constructor
-	
+	public Event(int eventId, String eventName, String typeOfEvent, String dateAndTime,  
+			String venue, int noOfParticipants, String description) {
+		this.eventId = eventId;
+		this.eventName = eventName;
+		this.typeOfEvent = typeOfEvent;
+		this.dateAndTime = dateAndTime;
+		this.venue = venue;
+		this.noOfParticipants = noOfParticipants;
+		this.description = description;
+	}
 	// getters and setters
+	public int getEventId() {
+		return this.eventId;
+	}
+	
+	public String eventName() {
+		return this.eventName;
+	}
+	
+	public String getType() {
+		return this.typeOfEvent;
+	}
+	
+	public String getDate() {
+		return this.dateAndTime;
+	}
+	
+	public String getVenue() {
+		return this.venue;
+	}
+	
+	public int getParticipants() {
+		return this.noOfParticipants;
+	}
+	
+	public String getDescription() {
+		return this.description;
+	}
 	
 	// METHODS
 	public void CreateEvent() { // up to the user in how the event is created
