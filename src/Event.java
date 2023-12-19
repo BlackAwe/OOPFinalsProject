2a1560fc08e1a745764a10f2b57c8a58a08c50ab
Justin <itinjus@gmail.com>
2023-12-19 21:18:25 +0800

    Filled and added the event class with update event method
    Update event method includes asking and prompting users if
    they want changes

--- src/Event.java
+++ src/Event.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 
 public class Event {
 	// class fields
 	private int eventId;
 	private String eventName;
-	private String typeOfEvent;
+	private String eventType;
 	private String dateAndTime;
 	private String venue;
 	private int noOfParticipants;
@@ -11,11 +12,11 @@ public class Event {
 	private String status;
 	
 	// constructor
-	public Event(int eventId, String eventName, String typeOfEvent, String dateAndTime,  
+	public Event(int eventId, String eventName, String eventType, String dateAndTime,  
 			String venue, int noOfParticipants, String description) {
 		this.eventId = eventId;
 		this.eventName = eventName;
-		this.typeOfEvent = typeOfEvent;
+		this.eventType = eventType;
 		this.dateAndTime = dateAndTime;
 		this.venue = venue;
 		this.noOfParticipants = noOfParticipants;
@@ -31,7 +32,7 @@ public class Event {
 	}
 	
 	public String getType() {
-		return this.typeOfEvent;
+		return this.eventType;
 	}
 	
 	public String getDate() {
@@ -54,6 +55,30 @@ public class Event {
 		return this.status;
 	}
 	
+	public void setName(String eventName) {
+		this.eventName = eventName;
+	}
+	
+	public void setType(String eventType) {
+		this.eventType = eventType;
+	}
+	
+	public void setDate(String dateAndTime) {
+		this.dateAndTime = dateAndTime;
+	}
+	
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	
+	public void setParticipants(int noOfParticipants) {
+		this.noOfParticipants = noOfParticipants;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
 	// METHODS
 	public void CreateEvent() { // up to the user in how the event is created
 		
@@ -70,8 +95,55 @@ public class Event {
 	}
 	
 	public void UpdateEvent() {
-		
-	}
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What would you like to update?");
+        System.out.println("1. Event Name");
+        System.out.println("2. Event Type");
+        System.out.println("3. Date and Time");
+        System.out.println("4. Venue");
+        System.out.println("5. Number of Participants");
+        System.out.println("6. Description");
+        System.out.print("Enter your choice (1-6): ");
+
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        switch (choice) {
+            case 1:
+                System.out.print("Enter new event name: ");
+                String newEventName = scanner.nextLine();
+                this.setName(newEventName);
+                break;
+            case 2:
+                System.out.print("Enter new event type: ");
+                String newEventType = scanner.nextLine();
+                this.setType(newEventType);
+                break;
+            case 3:
+                System.out.print("Enter new date and time: ");
+                String newDateAndTime = scanner.nextLine();
+                this.setDate(newDateAndTime);
+                break;
+            case 4:
+                System.out.print("Enter new venue: ");
+                String newVenue = scanner.nextLine();
+                this.setVenue(newVenue);
+                break;
+            case 5:
+                System.out.print("Enter new number of participants: ");
+                int newNoOfParticipants = scanner.nextInt();
+                this.setParticipants(newNoOfParticipants);
+                break;
+            case 6:
+                System.out.print("Enter new description: ");
+                String newDescription = scanner.nextLine();
+                this.setDescription(newDescription);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+        System.out.println("Event details updated successfully!");
+    }
 	
 	public void CancelEvent() {
 		
